bb5ab336bb29a153e4c8d63ca2d5e74c67133028
Simone <info@simoneicardi.com>
2026-02-23 11:32:52 +0100

    aggiungi router-dom e comp header

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 function App() {
 
     return (
-        <h1>Eccomi sono la nuova app di FE per i libri</h1>
+        <>
+            <h1>Eccomi sono la nuova app di FE per i libri</h1>
+        </>
+
+
 
     )
 }
