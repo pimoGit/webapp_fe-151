43946e412eb2220589cd16ead45b9562cf287111
Simone <info@simoneicardi.com>
2026-02-24 10:22:22 +0100

    dinamicizza cardbook con props e setta page home per invio dati finti

--- src/components/CardBook.jsx
+++ src/components/CardBook.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
 
-const CardBook = () => {
+const CardBook = (props) => {
+
+    const { id, title, author, abstract, image } = props.bookProp;
+
     return (
         <div className="card mb-4">
-            <img src="https://www.jdandj.com/uploads/8/0/0/8/80083458/s611371146998849390_p82_i2_w1600.jpeg"
-                className="card-img-top" alt="titolo del libro" />
+            <img src={image}
+                className="card-img-top" alt={title} />
             <div className="card-body">
-                <h5 className="card-title">Titolo del libro</h5>
-                <address><i>Autore</i></address>
-                <p className="card-text">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Soluta ratione veritatis sunt molestias nihil odit totam, quis corporis laboriosam excepturi. Labore vero molestiae eveniet explicabo esse iure laborum nemo odio.</p>
-                <Link to={`books/1`} className="btn btn-primary">See more</Link>
+                <h5 className="card-title">{title}</h5>
+                <address><i>By {author}</i></address>
+                <p className="card-text">{abstract}</p>
+                <Link to={`books/${id}`} className="btn btn-primary">See more</Link>
             </div>
         </div>
     )
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,69 @@
 import CardBook from "../components/CardBook"
 
 
+// dati temporanei per test props
+const books = [
+    {
+        "id": 1,
+        "title": "The Great Gatsby",
+        "author": "F. Scott Fitzgerald",
+        "abstract": "A novel about the American dream and the tragedy of idealism.",
+        "image": "great-gatsby.jpg",
+        "created_at": "2024-11-26T09:58:09.000Z",
+        "updated_at": "2024-12-20T10:30:54.000Z"
+    },
+    {
+        "id": 2,
+        "title": "To Kill a Mockingbird",
+        "author": "Harper Lee",
+        "abstract": "A story of racial injustice and moral growth in the Deep South.",
+        "image": "to_kill_a_mocking_bird.jpg",
+        "created_at": "2024-11-26T09:58:09.000Z",
+        "updated_at": "2024-12-20T10:30:54.000Z"
+    },
+    {
+        "id": 3,
+        "title": "1984",
+        "author": "George Orwell",
+        "abstract": "A dystopian tale of a totalitarian regime and the fight for freedom.",
+        "image": "1984.jpg",
+        "created_at": "2024-11-26T09:58:09.000Z",
+        "updated_at": "2024-12-20T10:30:54.000Z"
+    },
+    {
+        "id": 4,
+        "title": "Pride and Prejudice",
+        "author": "Jane Austen",
+        "abstract": "A romantic story about love and social standings.",
+        "image": "pride_and_prejudice.jpg",
+        "created_at": "2024-11-26T09:58:09.000Z",
+        "updated_at": "2024-12-20T10:28:01.000Z"
+    },
+    {
+        "id": 5,
+        "title": "Moby Dick",
+        "author": "Herman Melville",
+        "abstract": "A tale of obsession and revenge on the high seas.",
+        "image": "moby_dick.jpg",
+        "created_at": "2024-11-26T09:58:09.000Z",
+        "updated_at": "2024-12-20T10:30:54.000Z"
+    }
+];
+
 const HomePage = () => {
+
+    // funzione di rendering del listato dei libri
+    const rederBooks = () => {
+        return books.map(book => {
+            return (
+                <div className="col" key={book.id}>
+                    <CardBook bookProp={book} />
+                </div>
+            )
+        })
+    }
+
+
     return (
         <>
             <h1 className="text-primary">Bool Books</h1>
@@ -10,14 +72,7 @@ const HomePage = () => {
 
             <div>
                 <div className="row row-cols-3 mt-4">
-                    <CardBook />
-
-                    <CardBook />
-                    <CardBook />
-                    <CardBook />
-                    <CardBook />
-                    <CardBook />
-                    <CardBook />
+                    {rederBooks()}
                 </div>
             </div>
         </>
