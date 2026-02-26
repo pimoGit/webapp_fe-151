9ed4e3ddaade86a639e6ea62b9ce62926e8aa90f
Simone <info@simoneicardi.com>
2026-02-26 10:34:18 +0100

    crea comp loader e context globale

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -1,6 +1,12 @@
 import { Outlet } from "react-router-dom";
 
+// import dell'header
 import MainHeader from "../components/MainHeader";
+//  import del loader
+import Loader from "../components/Loader";
+
+// TEMP var di settaggio caricamento
+const isLoading = true;
 
 const DefaultLayout = () => {
     return (
@@ -9,6 +15,7 @@ const DefaultLayout = () => {
             <main className="container">
                 <Outlet />
             </main>
+            {isLoading && <Loader />}
         </>
     )
 }
